8889e36c49d29f51eb97d30caaaad55e85c8864e
virginiah2020 <vancyvy254@gmail.com>
2020-06-20 21:50:00 +0300

    styled h1 tag

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,6 @@
 export class User {
+    constructor(
+        public name: string,public login: string,public avatar_url: string,
+        public followers: string,public following: string,public public_repos: string){}
+    
 }

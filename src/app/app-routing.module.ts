15609b238213a9c20e28199373ae8d31b1af2391
virginiah2020 <vancyvy254@gmail.com>
2020-06-22 20:36:45 +0300

    imported Profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent},
+  { path: 'landingpage', component: LandingPageComponent},
+  { path:'**', component:LandingPageComponent},
+
+  { path: '', redirectTo:"/landingpage", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

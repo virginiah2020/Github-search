33943bbda17b88ae13cfd3099bd2394b866e5d38
virginiah2020 <vancyvy254@gmail.com>
2020-06-22 19:56:49 +0300

    added class navbar-toggler

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  clientid :"9cf258b6c15ba468ae60" ,
+  clientsecret : "b96aea5130b1946f4bfaefc5432dd16619660dbb",
+  
 };
 
 /*
